893e6b7747d2ed6ef462d3a0de54520a062bfdb1
sudharsan2027vec <sudharsanlabvec@gmail.com>
2025-04-01 16:20:57 +0530

    Add src/main/java/com/taskmanager/payload/response/JwtResponse.java 1743504657053

---
+++ src/main/java/com/taskmanager/payload/response/JwtResponse.java
@@ -0,0 +1,36 @@
+package com.taskmanager.payload.response;
+
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+
+    public JwtResponse(String token, Long id, String username, String email) {
+        this.token = token;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
